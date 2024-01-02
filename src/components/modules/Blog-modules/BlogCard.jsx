b3e7cc0b7180bf87b5fbb3c63bbede5683160b58
amiriar <amiriar.irr@gmail.com>
2024-01-02 18:34:46 +0330

    some problems fixed in blog

--- src/components/modules/Blog-modules/BlogCard.jsx
+++ src/components/modules/Blog-modules/BlogCard.jsx
@@ -1,29 +1,39 @@
 import React from 'react';
 import { Card, CardContent, CardHeader, Avatar, Typography, Divider } from '@mui/material';
+import './BlogCard.css'
 
-const BlogCard = ({ imageData, date, title, description, authorName, authorDescription, authorPicture }) => {
-    console.log(date);
+const BlogCard = ({ imageData, date, title, description, authorName, authorDescription, authorPicture, hashtags }) => {
+    console.log(hashtags);
     return (
-        <Card style={{ marginBottom: 20, borderRadius: '0.5rem', textAlign:"right" }}>
+        <Card className='needFont' style={{ marginBottom: 20, borderRadius: '0.5rem', textAlign:"right",boxShadow:"0px 4px 8px 0px rgba(0, 0, 0, 0.10)", position:"relative" }}>
             {/* Card Content */}
-            <CardContent>
+            <CardContent style={{ padding: 0 }}>
                 {/* Image */}
                 <div style={{ overflow: 'hidden', borderRadius: '0.5rem' }}>
                     <img src={imageData} alt="Card Image" style={{ aspectRatio:"2/1.5",width: '100%', height: 'auto', borderRadius: '0.5rem' }} />
                 </div>
 
                 {/* Date */}
-                <Typography variant="subtitle2" color="textSecondary" style={{ marginTop: 10 }}>
-                    {date || "test"}
+                <Typography variant="subtitle2" color="textSecondary" fontFamily={'Yekan, sans-serif'} style={{ marginTop: 8, marginRight:15 }}>
+                    {date || "date"}
                 </Typography>
 
                 {/* Title */}
-                <Typography variant="h5" component="div" style={{ marginTop: 10 }}>
+                <Typography variant="h5" component="div" fontFamily={'Yekan, sans-serif'} style={{ marginTop: 10,marginRight:15 }}>
                     {title}
                 </Typography>
 
+                {/* Hashtags */}
+                <div style={{ position: 'absolute', top: 15, right: 5}}>
+                    {hashtags && hashtags.map((tag, index) => (
+                        <Typography key={index} variant="body2" fontFamily={'Yekan, sans-serif'} style={{ display: 'inline', marginRight: 5, color: '#009688',background: 'rgba(0, 0, 0, 0.65)', borderRadius:"0.5rem",padding: '0.3rem 0.6rem' }}>
+                            #{tag}
+                        </Typography>
+                    ))}
+                </div>
+
                 {/* Description */}
-                <Typography variant="body2" color="textSecondary" component="p" style={{ marginTop: 10 }}>
+                <Typography variant="body2" color="textSecondary" component="p" fontFamily={'Yekan, sans-serif'} style={{ marginTop: 10, marginRight:15, marginBottom:10 }}>
                     {description}
                 </Typography>
             </CardContent>
@@ -35,8 +45,8 @@ const BlogCard = ({ imageData, date, title, description, authorName, authorDescr
             <CardContent style={{ direction:"rtl", display: 'flex', justifyContent: 'flex-start', alignItems: 'center', textAlign: 'right' }}>
                 <Avatar src={authorPicture} alt={authorName} style={{ marginLeft: 10 }} />
                 <div>
-                    <Typography variant="subtitle1">{authorName}</Typography>
-                    <Typography variant="body2" color="textSecondary">
+                    <Typography fontFamily={'Yekan, sans-serif'} variant="subtitle1">{authorName}</Typography>
+                    <Typography fontFamily={'Yekan, sans-serif'} variant="body2" color="textSecondary">
                         {authorDescription}
                     </Typography>
                 </div>
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -14,148 +14,97 @@ function Blog() {
     const cardData = [
         {
             id:1,
-            imageData:"/public/assets/prof.jpg",
+            imageData:"/public/assets/prof2.jpg",
             date: todaysDate,
-            title:"متن تست شماره 1",
-            description:"توضیحات تست 1",
+            title:"آناتومی یک صفحه وب و عناصر اساسی آن 1",
+            description:"لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون.",
             author:{
-                name:"نام نویسنده",
+                name:"حسین اشرفی پور",
                 picture:"/public/assets/prof.jpg",
-                description:"درباره نویسنده"
-            }
+                description:"طراح رابط کاربری و تجربه کاربر"
+            },
+            hashtags:['تکنولوژی','رابط کاربری']
         },
         {
             id:2,
-            imageData:"/public/assets/prof.jpg",
+            imageData:"/public/assets/prof2.jpg",
             date: todaysDate,
-            title:"متن تست شماره 2",
-            description:"توضیحات تست 1",
+            title:"آناتومی یک صفحه وب و عناصر اساسی آن 1",
+            description:"لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون.",
             author:{
-                name:"نام نویسنده",
-                picture:"/public/assets/prof2.jpg",
-                description:"درباره نویسنده"
-            }
+                name:"حسین اشرفی پور",
+                picture:"/public/assets/prof.jpg",
+                description:"طراح رابط کاربری و تجربه کاربر"
+            },
+            hashtags:['تکنولوژی','رابط کاربری']
         },
         {
             id:3,
-            imageData:"/public/assets/prof.jpg",
+            imageData:"/public/assets/prof2.jpg",
             date: todaysDate,
-            title:"متن تست شماره 3",
-            description:"توضیحات تست 1",
+            title:"آناتومی یک صفحه وب و عناصر اساسی آن 1",
+            description:"لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون.",
             author:{
-                name:"نام نویسنده",
-                picture:"/public/assets/prof2.jpg",
-                description:"درباره نویسنده"
-            }
+                name:"حسین اشرفی پور",
+                picture:"/public/assets/prof.jpg",
+                description:"طراح رابط کاربری و تجربه کاربر"
+            },
+            hashtags:['تکنولوژی','رابط کاربری']
         },
         {
             id:4,
-            imageData:"/public/assets/prof.jpg",
+            imageData:"/public/assets/prof2.jpg",
             date: todaysDate,
-            title:"متن تست شماره 3",
-            description:"توضیحات تست 1",
+            title:"آناتومی یک صفحه وب و عناصر اساسی آن 1",
+            description:"لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون.",
             author:{
-                name:"نام نویسنده",
-                picture:"/public/assets/prof2.jpg",
-                description:"درباره نویسنده"
-            }
+                name:"حسین اشرفی پور",
+                picture:"/public/assets/prof.jpg",
+                description:"طراح رابط کاربری و تجربه کاربر"
+            },
+            hashtags:['تکنولوژی','رابط کاربری']
         },
         {
             id:5,
-            imageData:"/public/assets/prof.jpg",
+            imageData:"/public/assets/prof2.jpg",
             date: todaysDate,
-            title:"متن تست شماره 3",
-            description:"توضیحات تست 1",
+            title:"آناتومی یک صفحه وب و عناصر اساسی آن 1",
+            description:"لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون.",
             author:{
-                name:"نام نویسنده",
-                picture:"/public/assets/prof2.jpg",
-                description:"درباره نویسنده"
-            }
+                name:"حسین اشرفی پور",
+                picture:"/public/assets/prof.jpg",
+                description:"طراح رابط کاربری و تجربه کاربر"
+            },
+            hashtags:['تکنولوژی','رابط کاربری']
         },
         {
             id:6,
-            imageData:"/public/assets/prof.jpg",
+            imageData:"/public/assets/prof2.jpg",
             date: todaysDate,
-            title:"متن تست شماره 3",
-            description:"توضیحات تست 1",
+            title:"آناتومی یک صفحه وب و عناصر اساسی آن 1",
+            description:"لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون.",
             author:{
-                name:"نام نویسنده",
-                picture:"/public/assets/prof2.jpg",
-                description:"درباره نویسنده"
-            }
+                name:"حسین اشرفی پور",
+                picture:"/public/assets/prof.jpg",
+                description:"طراح رابط کاربری و تجربه کاربر"
+            },
+            hashtags:['تکنولوژی','رابط کاربری']
         },
         {
             id:7,
-            imageData:"/public/assets/prof.jpg",
+            imageData:"/public/assets/prof2.jpg",
             date: todaysDate,
-            title:"متن تست شماره 3",
-            description:"توضیحات تست 1",
+            title:"آناتومی یک صفحه وب و عناصر اساسی آن 1",
+            description:"لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون.",
             author:{
-                name:"نام نویسنده",
-                picture:"/public/assets/prof2.jpg",
-                description:"درباره نویسنده"
-            }
+                name:"حسین اشرفی پور",
+                picture:"/public/assets/prof.jpg",
+                description:"طراح رابط کاربری و تجربه کاربر"
+            },
+            hashtags:['تکنولوژی','رابط کاربری']
         },
-        {
-            id:8,
-            imageData:"/public/assets/prof.jpg",
-            date: todaysDate,
-            title:"متن تست شماره 3",
-            description:"توضیحات تست 1",
-            author:{
-                name:"نام نویسنده",
-                picture:"/public/assets/prof2.jpg",
-                description:"درباره نویسنده"
-            }
-        },
-        {
-            id:9,
-            imageData:"/public/assets/prof.jpg",
-            date: todaysDate,
-            title:"متن تست شماره 3",
-            description:"توضیحات تست 1",
-            author:{
-                name:"نام نویسنده",
-                picture:"/public/assets/prof2.jpg",
-                description:"درباره نویسنده"
-            }
-        },
-        {
-            id:10,
-            imageData:"/public/assets/prof.jpg",
-            date: todaysDate,
-            title:"متن تست شماره 3",
-            description:"توضیحات تست 1",
-            author:{
-                name:"نام نویسنده",
-                picture:"/public/assets/prof2.jpg",
-                description:"درباره نویسنده"
-            }
-        },
-        {
-            id:11,
-            imageData:"/public/assets/prof.jpg",
-            date: todaysDate,
-            title:"متن تست شماره 3",
-            description:"توضیحات تست 1",
-            author:{
-                name:"نام نویسنده",
-                picture:"/public/assets/prof2.jpg",
-                description:"درباره نویسنده"
-            }
-        },
-        {
-            id:12,
-            imageData:"/public/assets/prof.jpg",
-            date: todaysDate,
-            title:"متن تست شماره 3",
-            description:"توضیحات تست 1",
-            author:{
-                name:"نام نویسنده",
-                picture:"/public/assets/prof2.jpg",
-                description:"درباره نویسنده"
-            }
-        },
+        
+        
 
     ]
 
@@ -166,13 +115,14 @@ function Blog() {
                 <Grid item key={index} xs={12} sm={6} md={4}>
                     {/* Adjust the xs, sm, and md values to control the number of cards per row */}
                     <BlogCard
-                    imageData={card.imageData}
-                    date={card.date}
-                    title={card.title}
-                    description={card.description}
-                    authorName={card.author.name}
-                    authorDescription={card.author.description}
-                    authorPicture={card.author.picture}
+                        imageData={card.imageData}
+                        date={card.date}
+                        title={card.title}
+                        description={card.description}
+                        authorName={card.author.name}
+                        authorDescription={card.author.description}
+                        authorPicture={card.author.picture}
+                        hashtags={card.hashtags}
                     />
                 </Grid>
                 ))}
