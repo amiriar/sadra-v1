f5e748852ffbf62da130d53887895142034836fe
amiriar <amiriar.irr@gmail.com>
2024-01-04 14:12:08 +0330

    home styles updated

---
+++ src/components/modules/ProgressBar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const ProgressBar = (props) => {
+  const { bgcolor, progress, width } = props;
+
+  const containerStyles = {
+    width: width,
+    height: "100%",
+    backgroundColor: "#e0e0de",
+    borderRadius: 50,
+    margin: 50,
+  };
+
+  const fillerStyles = {
+    width: "100%",
+    height: `${progress}%`,
+    backgroundColor: bgcolor,
+    borderRadius: "inherit",
+    textAlign: "right",
+    transition: "height 1s ease-in-out",
+  };
+
+  const labelStyles = {
+    padding: 5,
+    color: "white",
+    fontWeight: "bold",
+  };
+
+  return (
+    <div style={containerStyles} id="dissapear">
+      <div style={fillerStyles}>
+        <span style={labelStyles}>{`${progress}%`}</span>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -1,26 +1,110 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+
+//css:
+import './BlogDetails.css'
 
 // DB:
 import BlogDB from '../utils/BlogDB.json'
+import VerticalProgressBar from '../components/modules/ProgressBar'
+import { Avatar, CardContent, Divider, Typography } from '@mui/material'
+
+//icons:
+import { FaPinterest } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
 
 function BlogDetails() {
     const { id } = useParams()
     const blogId = parseInt(id, 10);
     const BlogPost = BlogDB.find((blog) => blog.id === blogId)
     return (
-        <div>FaArrowLeft
-            <div className='Home'>
-                <div className='image_container'>
-                    <img src={BlogPost.imageData} alt={BlogPost.imageData} />
+        <div>
+            <div className='Home' id='heroBlogDetails'  >
+                <div>
+                    <div className='image_container_blog_details' >
+                        <img src={BlogPost.imageData} alt={BlogPost.imageData} id='imageResponsive' />
+                    </div>
                 </div>
+                <div>
+                    <div className='Data_Container'>
+                        <div className='tags' dir='rtl' id='hashtagsDetails' style={{marginBottom:"1rem"}}>
+                            {
+                                BlogPost.hashtags.map((tag) => (
+                                    <Link className='link' to={`/${tag}`} key={tag}>#{tag}</Link>
+                                ))
+                            }
+                        </div>
+                        <h1 style={{color:"#F9FAFB !important"}} dir='rtl'>{BlogPost.title}</h1>
+                        <h2 style={{color:"#D0D5DD !important"}} dir='rtl'>{BlogPost.description}</h2>
+                        <p style={{color:"#FFF !important"}} dir='rtl'>نوشته شده توسط {BlogPost.author.name}</p>
+                    </div>
+                </div>
+            </div>
 
-                <div className='Data_Container'>
-                    <h1>مؤسسه آموزشی و پژوهشی صدرا</h1>
-                    <h2>دوره مورد علاقت رو شرکت کن، گارانتی پیدا کردن کار با ما</h2>
-                    <p>با شرکت در دوره‌های آموزشی صدرا، از صفر شروع کن و در مسیر یادگیری با بهترین متد‌های آموزشی ما همراه شو، تا ما پلی باشیم برای ورود تضمینی به بازار کار</p>
+            <div className='mainContentBlogDetails'>
+                <div className='ContentTextBlogDetails'>    
+                    <div>
+                        <p style={{color: "#475467"}} dir='rtl'>{BlogPost.detailsDescription1}</p>
+                        <p style={{color: "#475467"}} dir='rtl'>{BlogPost.detailsDescription2}</p>
+                        <p style={{color: "#475467"}} dir='rtl'>{BlogPost.detailsDescription3}</p>
+                    </div>
+                    <div className='BlogDetailsPictures'>
+                        <img src={BlogPost.descriptionImage1} alt={BlogPost.descriptionImage2} className='BlogDetailsPicture' />
+                        <img src={BlogPost.descriptionImage2} alt={BlogPost.descriptionImage2} className='BlogDetailsPicture' />
+                    </div>
+                    <div className='BlogDetailsSummery'>
+                        <p dir='rtl' style={{color: "#475467"}}>{BlogPost.detailsDescription4}</p>
+                        <h1 dir='rtl' style={{color:"#4CA773"}}>"{BlogPost.title}"</h1>
+                        <p dir='rtl' style={{color: "#475467"}}>{BlogPost.detailsDescription4}</p>
+                    </div>
+                    <div className='BlogPostInfo'>
+                        <div className='tags' id='hashtagsDetails' dir='rtl'>
+                            {
+                                BlogPost.hashtags.map((tag)=> (
+                                    <Link className='link' to={`/blog/${tag}`} style={{background: 'rgba(0, 0, 0, 0.65)',color:"#FFF"}}>#{tag}</Link>
+                                ))
+                            }
+                        </div>
+
+                        <Divider/>
+
+                        <div className='authorInfo'>
+                            <CardContent style={{ direction: "rtl", display: 'flex', justifyContent: 'flex-start', alignItems: 'center', textAlign: 'right' }}>
+                                <Avatar src={''} alt={'authorName'} style={{ marginLeft: 10, objectFit:'cover' }} />
+                                <div>
+                                    <div>
+                                        <Typography fontFamily={'Yekan, sans-serif'} variant="subtitle1" color={'#475467'}>
+                                            {'authorName'}
+                                        </Typography>
+                                        <Typography fontFamily={'Yekan, sans-serif'} variant="body2" color={"#98A2B3"} >
+                                            {'authorDescription'}
+                                        </Typography>
+                                    </div>
+                                </div>
+                            </CardContent>
+                            <div className='authorIcons'>
+                                <Link to={BlogPost.author.linkedin} ><FaLinkedin       size={25} color='#495057' /></Link>
+                                <Link to={BlogPost.author.pinterest}><FaPinterest      size={25} color='#495057' /></Link>
+                                <Link to={BlogPost.author.twitterX} ><FaSquareXTwitter size={25} color='#495057' /></Link>
+                                <Link to={BlogPost.author.facebook} ><FaFacebook       size={25} color='#495057' /></Link>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>dw
+                <div className='timeProgressBlogDetails' dir='rtl'>
+                    <span>1402/1/4</span>
+                    <span>----</span>
+                    <span>4 دقیقه</span>
+                    <VerticalProgressBar bgcolor={"#6AD095"} progress={"60"} width={"5px"}/>
+                    <span id="dissapear">پایان</span>
+                </div>
+            </div>
+            <div className=''>
+
+            </div>
+
         </div>
     )
 }
