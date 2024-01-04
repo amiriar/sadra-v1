1af06f8679dac04af184dd4121f64be7c0a4d651
amiriar <amiriar.irr@gmail.com>
2024-01-04 15:20:28 +0330

    help needed in blogdetail

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -7,7 +7,11 @@ import './BlogDetails.css'
 // DB:
 import BlogDB from '../utils/BlogDB.json'
 import VerticalProgressBar from '../components/modules/ProgressBar'
-import { Avatar, CardContent, Divider, Typography } from '@mui/material'
+import { Avatar, CardContent, Divider, Grid, Typography } from '@mui/material'
+
+// cards:
+import BlogCard from '../components/modules/Blog-modules/BlogCard';
+
 
 //icons:
 import { FaPinterest } from "react-icons/fa";
@@ -15,13 +19,21 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaFacebook } from "react-icons/fa";
 
+// variables:
+let counter = 0;
+
 function BlogDetails() {
     const { id } = useParams()
     const blogId = parseInt(id, 10);
     const BlogPost = BlogDB.find((blog) => blog.id === blogId)
+
+    const reliablePost1 = BlogDB.find((blog) => blog.id === 1)
+    const reliablePost2 = BlogDB.find((blog) => blog.id === 2)
+    const reliablePost3 = BlogDB.find((blog) => blog.id === 3)
+
     return (
-        <div>
-            <div className='Home' id='heroBlogDetails'  >
+        <div style={{ maxWidth:"1920px" ,margin:"0 auto"}}>
+            <div className='Home' id='heroBlogDetails'>
                 <div>
                     <div className='image_container_blog_details' >
                         <img src={BlogPost.imageData} alt={BlogPost.imageData} id='imageResponsive' />
@@ -63,23 +75,25 @@ function BlogDetails() {
                         <div className='tags' id='hashtagsDetails' dir='rtl'>
                             {
                                 BlogPost.hashtags.map((tag)=> (
-                                    <Link className='link' to={`/blog/${tag}`} style={{background: 'rgba(0, 0, 0, 0.65)',color:"#FFF"}}>#{tag}</Link>
+                                    <Link className='link' key={tag} to={`/blog/${tag}`} style={{background: 'rgba(0, 0, 0, 0.65)',color:"#FFF"}}>#{tag}</Link>
                                 ))
                             }
                         </div>
 
+                        <br />
                         <Divider/>
+                        <br />
 
                         <div className='authorInfo'>
                             <CardContent style={{ direction: "rtl", display: 'flex', justifyContent: 'flex-start', alignItems: 'center', textAlign: 'right' }}>
-                                <Avatar src={''} alt={'authorName'} style={{ marginLeft: 10, objectFit:'cover' }} />
+                                <Avatar src={BlogPost.author.picture} alt={'authorName'} style={{ marginLeft: 10, objectFit:'cover' }} />
                                 <div>
                                     <div>
                                         <Typography fontFamily={'Yekan, sans-serif'} variant="subtitle1" color={'#475467'}>
-                                            {'authorName'}
+                                            {BlogPost.author.name}
                                         </Typography>
                                         <Typography fontFamily={'Yekan, sans-serif'} variant="body2" color={"#98A2B3"} >
-                                            {'authorDescription'}
+                                            {BlogPost.author.description}
                                         </Typography>
                                     </div>
                                 </div>
@@ -92,17 +106,58 @@ function BlogDetails() {
                             </div>
                         </div>
                     </div>
+                    <div className='BlogRelatablePosts'>
+                        <h3 dir='rtl'>پست های مرتبط</h3>
+                        <div className='blogCardsContainer' dir='rtl'>
+                            {
+                                <Grid display={'flex'} container spacing={3}>
+                                    <Grid item key={reliablePost1.id} xs={12} sm={6} md={4}>
+                                        <BlogCard
+                                            id={reliablePost1.id}
+                                            imageData={reliablePost1.imageData}
+                                            date={reliablePost1.date}
+                                            title={reliablePost1.title}
+                                            description={reliablePost1.description}
+                                            authorName={reliablePost1.author.name}
+                                            authorDescription={reliablePost1.author.description}
+                                            authorPicture={reliablePost1.author.picture}
+                                            hashtags={reliablePost1.hashtags}
+                                        />
+                                        <BlogCard
+                                            id={reliablePost2.id}
+                                            imageData={reliablePost2.imageData}
+                                            date={reliablePost2.date}
+                                            title={reliablePost2.title}
+                                            description={reliablePost2.description}
+                                            authorName={reliablePost2.author.name}
+                                            authorDescription={reliablePost2.author.description}
+                                            authorPicture={reliablePost2.author.picture}
+                                            hashtags={reliablePost2.hashtags}
+                                        />
+                                        <BlogCard
+                                            id={reliablePost3.id}
+                                            imageData={reliablePost3.imageData}
+                                            date={reliablePost3.date}
+                                            title={reliablePost3.title}
+                                            description={reliablePost3.description}
+                                            authorName={reliablePost3.author.name}
+                                            authorDescription={reliablePost3.author.description}
+                                            authorPicture={reliablePost3.author.picture}
+                                            hashtags={reliablePost3.hashtags}
+                                        />
+                                    </Grid>
+                                </Grid>
+                            }
+                        </div>
+                    </div>
                 </div>
                 <div className='timeProgressBlogDetails' dir='rtl'>
                     <span>1402/1/4</span>
-                    <span>----</span>
+                    {/* <span><Line/></span> */}
                     <span>4 دقیقه</span>
                     <VerticalProgressBar bgcolor={"#6AD095"} progress={"60"} width={"5px"}/>
                     <span id="dissapear">پایان</span>
                 </div>
-            </div>
-            <div className=''>
-
             </div>
 
         </div>
