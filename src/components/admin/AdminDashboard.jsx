5d602e05d90ac829d2be659859db0c557e7bd90d
amiriar <amiriar.irr@gmail.com>
2024-01-07 14:57:23 +0330

    auth0 didnt work

--- src/components/admin/AdminDashboard.jsx
+++ src/components/admin/AdminDashboard.jsx
@@ -1,22 +1,9 @@
-import { useAuth0 } from '@auth0/auth0-react';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 const AdminDashboard = () => {
-    const { logout, isAuthenticated, user } = useAuth0()
-    console.log(user);
-
     return (
         <div>
-            {
-                isAuthenticated ?
-                <button className='login_Btn' style={{cursor:"pointer"}} onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>خروج</button>
-                :
-                <h1>به صفحه اصلی برگرد</h1>
-            }
-            <p>name: {user?.name}</p>
-            <p>nickname: {user?.nickname}</p>
-            <img src={user.picture} alt="pic" style={{height:"200px", width:"250px"}} />
+            
         </div>
     );
 };
