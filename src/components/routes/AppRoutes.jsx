b0b27fb9416b87ab8ed07e0cbb0249dcbb2706dc
RwinZakeri <rwiiinzakeri@gmail.com>
2024-01-07 12:10:00 +0330

    test

--- src/components/routes/AppRoutes.jsx
+++ src/components/routes/AppRoutes.jsx
@@ -5,20 +5,15 @@ import Blog from '../../pages/Blog';
 import BlogDetails from '../../pages/BlogDetails';
 import Events from '../../pages/Events';
 import StudentSuccess from '../../pages/StudentSuccess';
-<<<<<<< HEAD
-import EventDetail from '../../pages/EventDetail';
-import StudentDetail from '../../pages/StudentDetail';
-=======
 import StudentDetail from '../../pages/StudentDetail';
 import EventDetail from '../../pages/EventDetail';
->>>>>>> 10cb814bf0dfe4a8cf90eea19b5430415e5c52f9
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/blog/:id" element={<BlogDetails />} />
-            <Route path='/event/:id' element={<EventDetail/>} />
+            <Route path='/events/:id' element={<EventDetail/>} />
             <Route path="/events" element={<Events />} />
             <Route path="/classes" element={<Home />} />
             <Route path="/contact" element={<Home />} />
