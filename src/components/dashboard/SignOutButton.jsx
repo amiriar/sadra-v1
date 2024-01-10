116d7b40935e141a76a8745462327604056baa8d
amiriar <amiriar.irr@gmail.com>
2024-01-10 18:28:55 +0330

    Create SignOutButton.jsx

---
+++ src/components/dashboard/SignOutButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignOutButton = () => {
+    return (
+        <button>
+            خروج از حساب کاربری
+        </button>
+    );
+};
+
+export default SignOutButton;
