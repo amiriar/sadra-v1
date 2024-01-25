efc0383cf9945a030b193c15db14820916254872
RwinZakeri <rwiiinzakeri@gmail.com>
2024-01-25 09:40:46 +0330

    debugg

--- src/components/layouts/svg/MenuIcon.jsx
+++ src/components/layouts/svg/MenuIcon.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const MenuIcon = () => {
   return (
     <>
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
     <g id="Icon/Outline/Minus">
-    <path id="Vector" d="M3.75 12H20.25" stroke="#101828" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path id="Vector" d="M3.75 12H20.25" stroke="#101828" strokeWidth="1.5" strokeLinecap="round"/>
     </g>
     </svg>
 
